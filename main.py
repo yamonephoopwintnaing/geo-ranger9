6d12aec82211a62ba25918ba8854d677fe58a171
yamonephoopwintnaing <yamonenaing841995@gmail.com>
2018-07-24 11:49:44 +0630

    restart main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
-@app.route("/")
+myapp = Flask(__name__)
+@myapp.route("/")
 
 def main():
     return render_template("index.html")
             
-@app.route("/compose")
+@myapp.route("/compose")
 
 def compose():
     return render_template("compose.html")
 
 if __name__ == "__main__":
-app.run(debug=True)
+myapp.run(debug=True)
